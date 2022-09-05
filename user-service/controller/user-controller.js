871a5442b349396cf0b7e43202a785f7c67579ce
Daniel <cookiedan42@gmail.com>
2022-09-05 16:21:55 +0800

    add jwt to login path

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -1,5 +1,10 @@
 import { ormCreateUser as _createUser } from '../model/user-orm.js'
 import { ormLogin as _login } from '../model/user-orm.js'
+import {SignJWT,jwtVerify,generateKeyPair} from 'jose';
+
+//TODO: store keys in .env?
+const { publicKey, privateKey } = await generateKeyPair('ES256')
+
 
 export async function createUser(req, res) {
     try {
@@ -39,13 +44,28 @@ export async function login(req, res) {
             } else if (resp.err) {
                 return res.status(400).json({message: 'Could not login!'});
             } else {
-                console.log(`Successful login as ${username} !`)
-                return res.status(201).json({message: `Successful login as ${username} !`,JWT:`${username}_${password}_${new Date().getTime()}`});
+
+                let jwt_cookie = await new SignJWT({ })
+                .setProtectedHeader({ alg: 'ES256' })
+                .setIssuedAt()
+                .setIssuer('user-service')
+                .setExpirationTime('2h')
+                .sign(privateKey)
+
+                res.cookie('JWT',jwt_cookie,{httpOnly: true, secure: process.env.ENV == "PROD"});
+                return res.status(201).json({message: `Successful login as ${username} !`});
             }
         } else {
             return res.status(400).json({message: 'Username and/or Password are missing!'});
         }
     } catch (err) {
+        console.log(err)
         return res.status(500).json({message: 'Database failure when logging in!'})
     }
 }
+
+
+    // JWT decoder for validating cookie
+    // let { payload, protectedHeader } = await jwtVerify(jwt_cookie, publicKey, {
+    //     issuer: 'user-service',
+    //   })
