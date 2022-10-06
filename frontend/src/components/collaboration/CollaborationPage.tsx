101921f24746e7707f13911286e4e2645fc197fd
tanyjnaaman <tannaaman@hotmail.com>
2022-10-06 18:11:28 +0800

    Get code editor working

--- frontend/src/components/collaboration/CollaborationPage.tsx
+++ frontend/src/components/collaboration/CollaborationPage.tsx
@@ -1,9 +1,15 @@
-import { User } from "../../interfaces/users/User";
 import { useEffect, useState } from "react";
 import { Room } from "../../interfaces/collaboration/Room";
 import { apiGetRoom } from "../../api/CollaborationServiceApi";
 import useIsMobile from "../../hooks/useIsMobile";
 import { useSearchParams } from "react-router-dom";
+
+import Editor from "react-simple-code-editor";
+
+import { highlight, Grammar, languages } from "prismjs";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism.css";
 
 function CollaborationPage() {
   // =========== query params ==================
@@ -19,6 +25,11 @@ function CollaborationPage() {
   const isMobile = useIsMobile();
   const [isErrorSnackbarOpen, setIsErrorSnackbarOpen] = useState(false);
   const [errorSnackBarContent, setErrorSnackbarContent] = useState("");
+  const [codeLanguage, setCodeLanguage] = useState<{
+    languageGrammer: Grammar;
+    language: string;
+  }>({ languageGrammer: languages.python, language: "py" });
+  const [code, setCode] = useState("");
 
   // =========== hooks =========================
   /**
@@ -62,13 +73,32 @@ function CollaborationPage() {
   }, [roomId]);
 
   // =========== components ====================
-  const codeEditorComponent = <div></div>;
+  const codeEditorComponent = (
+    <Editor
+      value={code}
+      onValueChange={(code: string) => {
+        setCode(code);
+        if (room) {
+          room.state.code = code;
+          setRoom(room);
+          console.log("set room");
+        }
+      }}
+      highlight={(code) => highlight(code, languages.js, "js")}
+      padding={10}
+      style={{
+        fontFamily: '"Fira code", "Fira Mono", monospace',
+        fontSize: 12,
+      }}
+      tabSize={4}
+    />
+  );
   const chatComponent = <div></div>;
   const questionComponent = <div></div>;
   console.log(room);
   console.log(roomId);
 
-  return <div>Hello</div>;
+  return <div>{codeEditorComponent}</div>;
 }
 
 export default CollaborationPage;
